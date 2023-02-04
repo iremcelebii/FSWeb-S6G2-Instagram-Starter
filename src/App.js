61c28c54ae17443fe64ccb4e8c126a0e39240d67
İrem Çelebi <iremcelebi2404@gmail.com>
2023-02-05 01:55:51 +0300

    bitti ama bazı sorularım var

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-/* 
-  Buradan başlayın ve iç içe geçmiş bileşenlere doğru ilerleyin.
-  Projedeki tüm dosyalara kod eklenmesi gerekmez.
-  Nerelerde değişiklik yapmanız gerektiğini anlamak için her dosyayı inceleyin.
-*/
-
-// State hook u import edin
-import React from 'react';
-
-// Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
-// sahteVeri'yi import edin
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+import sahteVeri from "./sahte-veri";
 
 const App = () => {
-  // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
-  // Artık sahteVeri'ye ihtiyacınız olmayacak.
-  // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [arama, setArama] = useState(null);
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
+  const gonderiyiBegen = (gonderiID) => {
+    gonderiler.forEach((item) => {
+      if (item.id === gonderiID) {
+        item.likes += 1;
+        setGonderiler([...gonderiler]);
+      }
+    });
+  };
 
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
+  const aramaYap = (yeniArama) => {
+    setArama(yeniArama);
+
+    setGonderiler(
+      sahteVeri.filter((item) => item.username.includes(yeniArama))
+    );
+    // const arr = [];
+    // gonderiler.forEach((item) => {
+    //   if (item.username.includes(yeniArama)) {
+    //     arr.push(item);
+    //   }
+    //   setGonderiler([arr]);
+    // });
   };
 
   return (
-    <div className='App'>
-      {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
-      {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+    <div className="App">
+      <AramaÇubuğu arama={arama} aramaYap={aramaYap} />
+      <Gönderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
     </div>
   );
 };
